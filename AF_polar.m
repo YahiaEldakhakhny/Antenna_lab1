c7fda08888710e804d613dc2af84bffdff93d411
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-10-21 22:11:39 +0300

    AF_polar no errors but wrong graph

---
+++ AF_polar.m
@@ -0,0 +1,16 @@
+% This function plots the normalized Array factor pattern in polar co-ordinates in 2D
+function AF_polar(N, alpha, d_lamda_ratio)
+    % Define constants
+    step = 0.01;
+    gamma = -pi:step:pi;
+    psi = alpha + 2*pi*d_lamda_ratio * cos(gamma);
+
+    AF = sin(N*psi/2) ./ (N * sin(psi/2));
+    AF = abs(AF);
+
+    figure
+    polarplot(psi, AF)
+    pax = gca;
+    pax.ThetaZeroLocation = 'bottom';
+end
+

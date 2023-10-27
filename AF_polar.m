022a87ed39df941fa498ef9fbf1e0f7d4b12565f
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-10-27 19:16:36 +0200

    I give up on this branch

--- AF_polar.m
+++ AF_polar.m
@@ -1,7 +1,11 @@
 % This function plots the normalized Array factor pattern in polar co-ordinates in 2D
-function AF_polar(N, alpha, d_lamda_ratio)
+% function AF_polar(N, alpha, d_lamda_ratio)
     % Define constants
+    N = 6;
+    alpha = -5*pi/6; 
+    d_lamda_ratio = 5/12;
     step = 0.01;
+    
     gamma = -pi:step:pi;
     psi = alpha + 2*pi*d_lamda_ratio * cos(gamma);
 
@@ -11,6 +15,6 @@ function AF_polar(N, alpha, d_lamda_ratio)
     figure
     polarplot(psi, AF)
     pax = gca;
-    pax.ThetaZeroLocation = 'bottom';
-end
+%     pax.ThetaZeroLocation = 'bottom';
+% end
 

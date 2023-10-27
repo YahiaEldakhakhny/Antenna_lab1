fb8997cf091dc2b81e6c6991d641756be5da6461
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-10-27 19:36:47 +0200

    main part 2 all works but polar_2d

--- main.m
+++ main.m
@@ -3,3 +3,10 @@ L_Lamda_ratio = input("Enter length of dipole relative to lamda: ");
 E_plot_2D(L_Lamda_ratio);
 E_plot_3D(L_Lamda_ratio);
 
+%% Part 2: Uniform linear antenna array(ULA)
+d_lamda_ratio = input("Enter spacing w.r.t lamda (d): ");
+N = input("Enter number of elements (N): ");
+alpha = input("Enter the progressive shift (alpha): ");
+
+AF_cartesian(N, alpha, d_lamda_ratio);
+AF_cartesian_3D(N, alpha, d_lamda_ratio);
